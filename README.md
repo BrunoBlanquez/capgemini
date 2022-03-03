7756922c9c8046faba880a1387b9d2b0ff122d0e
Bruno <b.blanquez@yahoo.com.br>
2022-03-03 16:19:54 -0300

    lógica inicial da questão 2

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Linguagem Utilizada
 JavaScript
 
 🛠️ Instruções para abrir e rodar as lógicas JS
+# Questão 01
+O objetivo desta aplicação é retornar o número mediano em uma array de números ímpares. Para executar a aplicação, uma array de números ímpares deve ser passada como argumento da função.
+
+# Questão 02
+O objetivo desta aplicação é retornar a quantidade de elementos pares em uma determinada array, com uma diferença de X. Para executar a aplicação, primeiro é necessário definir o valor de X, depois os parâmetros que serão analisados
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// # Questão 01
 // A mediana de uma lista de números é basicamente o elemento que se encontra no meio da lista após a ordenação. Dada uma lista de números com um número ímpar de elementos, desenvolva um algoritmo que encontre a mediana.
 const mediana = (...array) => {
   if (array.length % 2 != 0) {
@@ -9,4 +10,33 @@ const mediana = (...array) => {
   else {
     alert("Insira uma sequência numérica impar")
   }
+}
+
+// #Questão 02
+//  Dado um vetor de inteiros "n" e um inteiro qualquer "x". Construa um algoritmo que determine o número de elementos pares do vetor que tem uma diferença igual ao valor de x.
+const diferencaDeX = (x, ...array) => {
+// Retorna valores com diferença de X
+let arrFinal = []
+let cont = 0
+let arrayInput = array
+
+while(cont < arrayInput.length) {
+  for (let i of arrayInput) {
+    if ((array[i] - arrayInput[cont]) === x) {
+      let numer1 = Number (arrayInput[cont])
+      let numer2 = Number (array[i])
+      arrFinal.push(numer1, numer2)
+    }
+  }
+cont++
+}
+// Verificar se os números são pares
+let numPares = []
+for (const num of arrFinal) {
+  if (num % 2 == 0) {
+    numPares.push(num)
+  }
+}
+// Remove os repetidos e coloca em ordem númerica
+return [...new Set(numPares)].sort((a,b) => {return a- b})
 }
